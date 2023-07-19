66d1b1f1a5a9c350fccd3e3729320fc61519ee13
monika2420 <138773088+monika2420@users.noreply.github.com>
2023-07-19 10:09:23 +0530

    Add files via upload

---
+++ src/About.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const About: React.FC = () => {
+  return (
+    <div>
+      <h2>This is the About section</h2>
+    </div>
+  );
+};
+
+export default About;
---
+++ src/Profile.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Profile: React.FC = () => {
+  return (
+    <div>
+      <h2>This is the Profile section</h2>
+    </div>
+  );
+};
+
+export default Profile;
